a6d765892e060072b848acc49c7277d52b348df4
Julio Cesar Fausto <jcfausto@gmail.com>
2017-04-29 10:10:03 +0200

    Todo items list base style. No buttoms yet.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('js loaded');
+//Load this script at the bottom of your body tag to make sure
+//the DOM is completed loaded
+
+var addButtom = document.getElementById('add');
+var item = document.getElementById('item');
+
+addButtom.addEventListener('click', function(){
+  var value = item.value;
+
+  if (value) {
+      //add to the todo list
+  }
+
+});
