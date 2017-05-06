43b2641f06cc231130a5324fe24cc1a6ccbada42
Julio Cesar Fausto <jcfausto@gmail.com>
2017-05-06 19:07:41 +0200

    Addded babel to use ES6. Added creation date to items. Storing json objects insted of just plain text. Added moment.js to handle dates. Refactored some code duplications

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
 //Telling webpack about this script dependencies
 import {deleteButtonSVG, completeButtonSVG} from './svgs.js';
+var moment = require('moment');
 
 var addButton = document.getElementById('add');
 var deleteButton = document.getElementById('delete');
@@ -10,7 +11,6 @@ var completeButton = document.getElementById('complete');
 var todoList = document.getElementById('todoList');
 var completedTodos = document.getElementById('completedTodos');
 var newTodo = document.getElementById('newTodo');
-
 
 //Try to get the object from local storage. If it's null, then initializa an empty object
 var data = (localStorage.getItem('todolist')) ? JSON.parse(localStorage.getItem('todolist')) : {
@@ -24,11 +24,17 @@ loadTodoList(data);
 addButton.addEventListener('click', function() {
   var value = newTodo.value;
   resetInputText();
-  addTodoItem(value);
 
-  //Store data
-  data.todo.push(value);
-  storeData(data);
+  var newItem = buildNewItem(value);
+
+  if (newItem) {
+    addTodoItem(newItem);
+
+    //Store data
+    data.todo.push(newItem);
+    storeData(data);
+  }
+
 });
 
 //To handle when the user pressed enter
@@ -36,11 +42,16 @@ newTodo.addEventListener('keypress', function(event) {
   if (event.which == 13 || event.keyCode == 13) {
     var value = newTodo.value;
     resetInputText();
-    addTodoItem(value);
 
-    //Store data
-    data.todo.push(value);
-    storeData(data);
+    var newItem = buildNewItem(value);
+
+    if (newItem) {
+      addTodoItem(newItem);
+
+      //Store data
+      data.todo.push(newItem);
+      storeData(data);
+    }
 
     return false;
   }
@@ -57,6 +68,7 @@ function resetInputText(){
 
 function storeData(data){
   localStorage.setItem('todolist', JSON.stringify(data));
+  console.log(data);
 }
 
 function loadTodoList(data) {
@@ -64,17 +76,30 @@ function loadTodoList(data) {
   if (!data.todo.length && !data.completed.length) return;
 
   for (var i = 0; i < data.todo.length; i++) {
-    var value = data.todo[i];
-    addTodoItem(value);
+    var item = data.todo[i];
+    addTodoItem(item);
   }
 
 
   for (var i = 0; i < data.completed.length; i++) {
-    var value = data.completed[i];
-    addTodoItem(value, true);
+    var item = data.completed[i];
+    addTodoItem(item, true);
   }
 
-  console.log(data);
+}
+
+//Requires a value. Returns false if no value is provided.
+function buildNewItem(value) {
+  if (value) {
+    var now = moment().format();
+
+    return {
+      value: value,
+      created: now
+    };
+
+  }
+  return false;
 }
 
 /* end of helper functions */
@@ -82,9 +107,9 @@ function loadTodoList(data) {
 /* Core user tasks */
 
 //Task: Adding a new todo item
-function addTodoItem(text, isCompleted) {
+function addTodoItem(item, isCompleted) {
   //Text should not be empty.
-  if (text) {
+  if (item) {
     //Every todo item should have a delete and a complete button.
     var itemButtons = document.createElement('div');
     itemButtons.classList.add('buttons');
@@ -104,7 +129,14 @@ function addTodoItem(text, isCompleted) {
 
     //New items should got to the top of the list.
     var todo = document.createElement('li');
-    todo.innerText = text;
+    todo.innerText = item.value;
+
+    var created = document.createElement('span');
+    created.classList.add('createdAt');
+    created.title = item.created;
+    created.innerText = moment(item.created).format('lll');
+
+    todo.appendChild(created);
     todo.appendChild(itemButtons);
 
     var targetList = (isCompleted) ? completedTodos : todoList;
@@ -116,7 +148,7 @@ function addTodoItem(text, isCompleted) {
 //Task: Deleting todo item
 function deleteTodo(){
   var item = this.parentNode.parentNode;
-  var value = item.innerText;
+  var value = item.firstChild.data;
   var parent = item.parentNode;
   var id = parent.id;
 
@@ -124,7 +156,10 @@ function deleteTodo(){
 
   //Store data
   var dataSource = (id === 'todoList') ? data.todo : data.completed;
-  dataSource.splice(dataSource.indexOf(value), 1);
+
+  var objIndex = dataSource.findIndex(item => item.value === value);
+  dataSource.splice(objIndex, 1);
+
   storeData(data);
 }
 
@@ -133,7 +168,7 @@ function deleteTodo(){
 function toggleTodo() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
-  var value = item.innerText;
+  var value = item.firstChild.data;
   var id = parent.id;
 
   var dataSource = (id === 'todoList') ? data.todo : data.completed;
@@ -144,7 +179,11 @@ function toggleTodo() {
   targetList.insertBefore(item, targetList.childNodes[0]);
 
   //Store data
-  dataSource.splice(dataSource.indexOf(value), 1);
-  dataTarget.push(value);
+  var objIndex = dataSource.findIndex(item => item.value === value);
+  var obj = dataSource[objIndex];
+
+  dataSource.splice(objIndex, 1);
+  dataTarget.push(obj);
+
   storeData(data);
 }
