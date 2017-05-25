6879f9502cdb31aedc68d7bac8b66ef53ce235ae
Julio Cesar Fausto <jcfausto@gmail.com>
2017-05-25 10:49:16 +0200

    Added mLab credentials and connecting to db

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var config = require('./config/config');
 
 var app = express();
 
 //This app is basic static files right now.
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(config.port, function(err){
-  if (err) throw err;
-  console.log('Running on port ' + config.port);
+mongoose.connect(config.db);
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to ' + config.db);
+
+  //I'll only start the app if I can connect properly to DB
+  app.listen(config.port, function(err){
+    if (err) throw err;
+    console.log('Running on port ' + config.port);
+  });
+
 });
+var db = mongoose.connection;
 
 //Exporting app to be used anywere
 module.exports = app;
--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
 var config = {
-  port: process.env.PORT || 2000
+  port: process.env.PORT || 2000,
+  db: process.env.MONGODB_URI || 'mongodb://tododbDevuser:agesune1@ds151431.mlab.com:51431/tododb_dev',
+  test_port: 2001,
+  test_db: 'mongodb://tododbTstuser:agesune1@ds151941.mlab.com:51941/tododb_test'
 };
 module.exports = config;
